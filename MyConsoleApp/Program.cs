6bce90bd4f246b29c98c9ecc2eabbfd847052278
Radoslav <117578667+Hallucinationl2@users.noreply.github.com>
2025-05-30 23:32:28 +0300

    Add files via upload

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class GuessGame
+{
+    static void Main()
+    {
+        Random rng = new Random();
+        int number = rng.Next(1, 101);
+        int guess, tries = 0;
+
+        Console.WriteLine("Познай числото между 1 и 100");
+        do
+        {
+            Console.Write("Вашият избор: ");
+            guess = int.Parse(Console.ReadLine());
+            tries++;
+
+            if (guess < number) Console.WriteLine("По-голямо!");
+            else if (guess > number) Console.WriteLine("По-малко!");
+            else Console.WriteLine($"Познахте за {tries} опита!");
+        } while (guess != number);
+    }
+}
